cf6617e773009946f8201d499e4a9214b2b3a691
Norah Hannah Schat <nschat@student.codam.nl>
2021-09-02 16:33:29 +0200

    fix: use libft functions in main

--- src/main.c
+++ src/main.c
@@ -6,18 +6,11 @@
 /*   By: nschat <nschat@student.codam.nl>             +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2021/08/04 15:18:00 by nschat        #+#    #+#                 */
-/*   Updated: 2021/08/19 14:42:46 by nschat        ########   odam.nl         */
+/*   Updated: 2021/09/02 16:32:56 by nschat        ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
-
-static int	isdigit(int c)
-{
-	if (c >= '0' && c <= '9')
-		return (1);
-	return (0);
-}
 
 static int	str_isnumeric(const char *str)
 {
@@ -27,7 +20,7 @@ static int	str_isnumeric(const char *str)
 		str++;
 	while (*str != '\0')
 	{
-		if (isdigit(*str) == 0)
+		if (ft_isdigit(*str) == 0)
 			return (0);
 		str++;
 	}
@@ -39,13 +32,13 @@ int	main(int ac, char **av)
 	int	i;
 
 	if (ac == 1)
-		write(2, "Error: not enough arguments\n", 28);
+		ft_putendl_fd("Error: not enough arguments", 2);
 	i = 1;
 	while (i < ac)
 	{
 		if (str_isnumeric(av[i]) != 1)
 		{
-			write(2, "Error: input is non-numeric\n", 28);
+			ft_putendl_fd("Error: input is non-numeric", 2);
 			return (1);
 		}
 		i++;
